b949576cf8af63b33075873b5a2e155db456672c
seedevk8s <seedevk8s@gmail.com>
2024-04-15 04:48:17 +0000

    함수 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World KOSTA!</h1>
+      <Welcome />
+      <Welcome />
     </div>
   );
 }
---
+++ src/components/Welcome.js
@@ -0,0 +1,5 @@
+function Welcome() {
+    return <h1>환영합니다. 호진!</h1>
+}
+
+export default Welcome
