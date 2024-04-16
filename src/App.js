9054b889f5298a6505e34e5e7bb0607e9897c400
seedevk8s <seedevk8s@gmail.com>
2024-04-16 06:09:10 +0000

    컴포넌트 임포트 및 익스포트하기

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import Welcome from './components/Welcome';
 import Greeting from './components/Welcome2';
 import Welcome3 from './components/Welcome3';
 import Goodbye from './components/Goodbye';
+// import { FirstComponent, SecondComponent } from './components/MyComponent';
+// import { FirstComponent as FC, SecondComponent } from './components/MyComponent';
+import { FirstComponent as FC, SecondComponent as SC } from './components/MyComponent';
+// import MyComponent from './components/MyComponent';
+import MC from './components/MyComponent';
 
 function App() {
   return (
@@ -15,6 +20,12 @@ function App() {
       <Welcome3 name="hojin" />
       <Goodbye />
       <Goodbye name="chu" />
+      {/* <FirstComponent /> */}
+      <FC />
+      {/* <SecondComponent /> */}
+      <SC />
+      {/* <MyComponent /> */}
+      <MC />
     </div>
   );
 }
---
+++ src/components/MyComponent.js
@@ -0,0 +1,15 @@
+export function FirstComponent() {
+    return <h1>첫번째 컴포넌트</h1>
+}
+
+export function SecondComponent() {
+    return <h1>두번째 컴포넌트</h1>
+}
+
+// export default FirstComponent
+
+function MyComponent() {
+    return <h1>나의 컴포넌트</h1>
+}
+
+export default MyComponent
