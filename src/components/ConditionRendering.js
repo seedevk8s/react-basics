ca97b5f7be530f7b0ab8748d6015839d7a4e37c7
seedevk8s <seedevk8s@gmail.com>
2024-04-25 14:54:06 +0000

    반환값 변수에 담아서  조건에 따라 분기되는 JSX문 작성하기

--- src/components/ConditionRendering.js
+++ src/components/ConditionRendering.js
@@ -2,13 +2,20 @@ import { useState } from "react"
 
 const ConditionRedering = () => {
 
-    const [isLoggedIn, setIsLoggedIn] = useState(true)
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
 
+    let message
     if(isLoggedIn) {
-        return <p>환영합니다. 사용자님!</p>
+        message = /* return */ <p>환영합니다. 사용자님!</p>
     } else {
-        return <p>부디 로그인하삼!</p>
+        message = /* return */ <p>부디 로그인하삼!</p>
     }
+
+    return (
+        <div>
+            {message}
+        </div>
+    )
 
 }
 
